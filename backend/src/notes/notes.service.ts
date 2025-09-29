b4756403d0e1d409bd1a780e9cfc50ba50caf63c
gabbu <gabrielle.sote@gmail.com>
2025-09-29 02:13:29 -0300

    fix: consertando erro de lógica para a api funcionar com o front

--- backend/src/notes/notes.service.ts
+++ backend/src/notes/notes.service.ts
@@ -1,9 +1,9 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { Note } from './entities/note.entity';
 import { CreateNoteDto } from './dto/create-note.dto';
 import { UpdateNoteDto } from './dto/update-note.dto';
-import { Note } from './entities/note.entity';
 
 @Injectable()
 export class NotesService {
@@ -12,38 +12,43 @@ export class NotesService {
     private readonly notesRepository: Repository<Note>,
   ) {}
 
+  // Simplesmente retorna o que o repositório criar. O id já será uma string (uuid).
   create(createNoteDto: CreateNoteDto): Promise<Note> {
-    const note = this.notesRepository.create(createNoteDto);
-    return this.notesRepository.save(note);
+    const newNote = this.notesRepository.create(createNoteDto);
+    return this.notesRepository.save(newNote);
   }
 
+  // Simplesmente retorna o que o repositório encontrar. O id já será uma string.
   findAll(): Promise<Note[]> {
     return this.notesRepository.find();
   }
 
+  // Recebe id como string e busca. O id já será uma string.
   async findOne(id: string): Promise<Note> {
     const note = await this.notesRepository.findOneBy({ id });
     if (!note) {
-      throw new NotFoundException(`Note with ID "${id}" not found`);
+      throw new NotFoundException(`Note with ID ${id} not found`);
     }
     return note;
   }
-
+  
+  // Recebe id como string e atualiza. O id já será uma string.
   async update(id: string, updateNoteDto: UpdateNoteDto): Promise<Note> {
     const note = await this.notesRepository.preload({
       id: id,
       ...updateNoteDto,
     });
     if (!note) {
-      throw new NotFoundException(`Note with ID "${id}" not found`);
+      throw new NotFoundException(`Note with ID ${id} not found`);
     }
     return this.notesRepository.save(note);
   }
 
+  // Recebe id como string e deleta.
   async remove(id: string): Promise<void> {
     const result = await this.notesRepository.delete(id);
     if (result.affected === 0) {
-      throw new NotFoundException(`Note with ID "${id}" not found`);
+      throw new NotFoundException(`Note with ID ${id} not found`);
     }
   }
 }
